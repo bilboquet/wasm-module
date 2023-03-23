4173e1d70d6ad9843f2161dada17281d43d4e988
Jean-François Morcillo <jfm@massa.net>
2023-03-23 16:59:37 +0100

    Add a function start() that calls an external function echo()
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 // `export` tells that we provide
 wai_bindgen_rust::export!("wasm-mod-exported.wai");
 
+// what we are using from the outside
+wai_bindgen_rust::import!("host-exported.wai");
+
+
 // looks like wai_bindgen_rust::export is buggy on this point it forget to declare this struct
 // it's documented in the example
 struct WasmModExported;
@@ -10,5 +14,10 @@ struct WasmModExported;
 impl wasm_mod_exported::WasmModExported for WasmModExported {
     fn add(a: u32, b: u32) -> u32 {
         a + b
+    }
+
+    fn start() -> u32 {
+        host_exported::echo();
+        0u32
     }
 }
